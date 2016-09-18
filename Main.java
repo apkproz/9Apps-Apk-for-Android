00f8898e0ff8cdb3e44f76380948791a04fadddd
help.azap@gmail.com <help.azap@gmail.com>
2016-09-18 23:42:34 +0530

    Added Main File

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Android{
+String n="9Apps is a free Store for Android Applications and Games";
+
+} 
+class Main{
+
+public static void main(String args[]){
+	
+	System.out.println(n);
+}
+
+}
